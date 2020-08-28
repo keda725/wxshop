3086563efc4ea59eeb2e3a4e4b5bc4afb48af292
keda725 <kkeda725@gmail.com>
2020-08-28 17:45:14 +0800

    验证输入的参数是否合法 合法 发送验证码 不合法 返回400

--- src/main/java/com/github/kb/wxshop/controller/AuthController.java
+++ src/main/java/com/github/kb/wxshop/controller/AuthController.java
@@ -1,12 +1,18 @@
 package com.github.kb.wxshop.controller;
 
 import com.github.kb.wxshop.service.AuthService;
+import com.github.kb.wxshop.service.TelVerificationService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
 
 /**
  * @author zuojiabin
@@ -15,21 +21,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class AuthController {
     private final AuthService authService;
+    private final TelVerificationService telVerificationService;
 
 
-    public AuthController(final AuthService authService) {
+    public AuthController(AuthService authService, TelVerificationService telVerificationService) {
         this.authService = authService;
+        this.telVerificationService = telVerificationService;
     }
 
     @PostMapping("/code")
-    public void code(@RequestBody final TelAndCode telAndCode) {
-        this.authService.sendVerificationCode(telAndCode.getTel());
-
+    public void code(@RequestBody TelAndCode telAndCode, HttpServletResponse response) {
+        if (telVerificationService.verifyTelParameter(telAndCode)) {
+            authService.sendVerificationCode(telAndCode.getTel());
+        } else {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+        }
     }
 
     @PostMapping("/login")
-    public void login(@RequestBody final TelAndCode telAndCode) {
-        final UsernamePasswordToken token = new UsernamePasswordToken(telAndCode.getTel(), telAndCode.getCode());
+    public void login(@RequestBody TelAndCode telAndCode) {
+        UsernamePasswordToken token = new UsernamePasswordToken(telAndCode.getTel(), telAndCode.getCode());
         token.setRememberMe(true);
         SecurityUtils.getSubject().login(token);
     }
