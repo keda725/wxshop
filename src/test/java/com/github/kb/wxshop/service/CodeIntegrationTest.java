b7fa435f4ec975da641cc91bd89355f9a5e22d30
keda725 <kkeda725@gmail.com>
2020-08-29 23:45:34 +0800

    发送验证码的集成测试

--- src/test/java/com/github/kb/wxshop/service/CodeIntegrationTest.java
+++ src/test/java/com/github/kb/wxshop/service/CodeIntegrationTest.java
@@ -1,2 +1,56 @@
-package com.github.kb.wxshop.service;public class CodeIntegrationTest {
+package com.github.kb.wxshop.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.kb.wxshop.WxshopApplication;
+import com.github.kevinsawicki.http.HttpRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
+import static java.net.HttpURLConnection.HTTP_OK;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = WxshopApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application.yml")
+public class CodeIntegrationTest {
+    @Autowired
+    Environment environment;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void returnHttpOKWhenParameterIsCorrect() throws JsonProcessingException {
+        int responseCode = HttpRequest.post(getUrl("/api/code"))
+                                      .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                      .accept(MediaType.APPLICATION_JSON_VALUE)
+                                      .send(objectMapper.writeValueAsString(TelVerificationServiceTest.VALID_PARAMETER))
+                                      .code();
+
+        Assertions.assertEquals(HTTP_OK, responseCode);
+    }
+
+    @Test
+    public void returnHttpBadRequestWhenParameterIsCorrect() throws JsonProcessingException {
+        int responseCode = HttpRequest.post(getUrl("/api/code"))
+                                      .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                      .accept(MediaType.APPLICATION_JSON_VALUE)
+                                      .send(objectMapper.writeValueAsString(TelVerificationServiceTest.EMPTY_TEL))
+                                      .code();
+
+        Assertions.assertEquals(HTTP_BAD_REQUEST, responseCode);
+    }
+
+    private String getUrl(String apiName) {
+        // 获取集成测试的端口号
+        return "http://localhost:" + environment.getProperty("local.server.port") + apiName;
+    }
 }
+
