71cb2f6b6af088fa13b33d2335cb18bd1102e3e2
keda725 <kkeda725@gmail.com>
2020-08-29 23:10:50 +0800

    生成jacoco可视化报告 并使TelVerificationService的测试率为百分之百

--- src/test/java/com/github/kb/wxshop/service/TelVerificationServiceTest.java
+++ src/test/java/com/github/kb/wxshop/service/TelVerificationServiceTest.java
@@ -7,10 +7,17 @@ import org.junit.jupiter.api.Test;
 
 class TelVerificationServiceTest {
     private static AuthController.TelAndCode VALID_PARAMETER = new AuthController.TelAndCode(null, "13800000000");
+    private static AuthController.TelAndCode EMPTY_TEL = new AuthController.TelAndCode(null, null);
 
     @Test
     public void returnTrueIfValid() {
         Assertions.assertTrue(new TelVerificationService().verifyTelParameter(VALID_PARAMETER));
     }
 
+
+    @Test
+    public void returnFalseIfNoTel() {
+        Assertions.assertFalse(new TelVerificationService().verifyTelParameter(EMPTY_TEL));
+        Assertions.assertFalse(new TelVerificationService().verifyTelParameter(null));
+    }
 }
